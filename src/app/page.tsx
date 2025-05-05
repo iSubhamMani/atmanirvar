3f98ff6d2d8fa8dbcc25c13aaf000de91aa69433
Ronit Bose <147628969+rbose3@users.noreply.github.com>
2025-05-05 12:33:43 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         >
           <Image src={icon} alt="आत्मनिर्भर ++ Logo" />
           <span className="ml-2 text-2xl font-bold text-secondary">
-            आत्मनिर्भर ++
+            SkillSarthi ++
           </span>
         </Link>
       </header>
@@ -121,7 +121,7 @@ export default function Home() {
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-muted-foreground">
-          &copy; 2024 आत्मनिर्भर ++. All rights reserved.
+          &copy; 2024 SkillSarthi ++. All rights reserved.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link
